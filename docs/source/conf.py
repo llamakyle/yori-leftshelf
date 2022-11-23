1e1ed7069b938e16f9ac59e171a282f17c3c4443
llamakyle <50254664+llamakyle@users.noreply.github.com>
2022-11-22 18:34:51 -0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'YORI'
+copyright = '2022, Kyle Gillespie'
+author = 'Kyle Gillespie'
 
 release = '0.1'
 version = '0.1.0'
